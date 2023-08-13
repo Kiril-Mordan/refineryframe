98dd886cc07dd9c314acc89ea0b04c5b06a2a1e3
Kiril-Mordan <kiril.mordan@icloud.com>
2023-08-14 00:30:20 +0200

    [DEBUG]

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'refineryframe package'
 copyright = '2023, Kyrylo Mordan'
