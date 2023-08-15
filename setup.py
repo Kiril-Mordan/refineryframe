57a84ab42981b6ce10cb748519e3b6b8427b06b4
GitHub Action <action@github.com>
2023-08-15 23:36:11 +0000

    Update README; setup.py; refineryframe files

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ base_packages = [
     "unidecode"
 ]
 
-VERSION = '0.1.21'
+VERSION = '0.1.22'
 DESCRIPTION = 'Cleans data, best to be used as a part of initial preprocessor'
 LONG_DESCRIPTION = 'A package that allows to detect unexpected values in data and clean them according to set of predefined rules'
 
